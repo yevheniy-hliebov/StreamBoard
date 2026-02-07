1a6c3ce7e3ba137ea166c91feb7c976ab9b7c5d5
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-02-07 22:12:23 +0200

    feat(settings): add SettingsPage and connect to navigation button

---
+++ Features/Settings/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StreamBoard.Features.Settings.Pages
+{
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
