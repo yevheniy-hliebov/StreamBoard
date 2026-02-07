a804a275e8adf046c47ea9016e7075889a4b93f4
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-02-07 20:50:54 +0200

    feat(components): add resuseble custom card component with toggle switch

---
+++ Components/Cards/CardWithToggleSwitch.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StreamBoard.Components.Cards
+{
+    public partial class CardWithToggleSwitch : UserControl
+    {
+        public CardWithToggleSwitch()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(CardWithToggleSwitch), new PropertyMetadata("Title"));
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(CardWithToggleSwitch), new PropertyMetadata("Description goes here"));
+
+        public string ToggleOffText
+        {
+            get { return (string)GetValue(ToggleOffTextProperty); }
+            set { SetValue(ToggleOffTextProperty, value); }
+        }
+        public static readonly DependencyProperty ToggleOffTextProperty =
+            DependencyProperty.Register("ToggleOffText", typeof(string), typeof(CardWithToggleSwitch), new PropertyMetadata("Off"));
+
+        public string ToggleOnText
+        {
+            get { return (string)GetValue(ToggleOnTextProperty); }
+            set { SetValue(ToggleOnTextProperty, value); }
+        }
+        public static readonly DependencyProperty ToggleOnTextProperty =
+            DependencyProperty.Register("ToggleOnText", typeof(string), typeof(CardWithToggleSwitch), new PropertyMetadata("On"));
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(CardWithToggleSwitch),
+                new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+    }
+}
