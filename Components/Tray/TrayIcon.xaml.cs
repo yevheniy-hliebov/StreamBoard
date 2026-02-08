bf3ac4c9b380d4833985fa863a956a091f8dbe73
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-02-08 09:44:00 +0200

    feat(ui): implement system tray icon with context menu

---
+++ Components/Tray/TrayIcon.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StreamBoard.Components.Tray
+{
+    public partial class TrayIconView : UserControl
+    {
+        public TrayIconView()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Application.Current.MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.Show();
+                mainWindow.WindowState = WindowState.Normal;
+                mainWindow.Activate();
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
