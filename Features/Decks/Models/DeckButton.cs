7a208e862476168add299adf33204bde8a382684
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-02-16 18:12:47 +0200

    feat(decks): implement DeckProfile and DeckButton data models

---
+++ Features/Decks/Models/DeckButton.cs
@@ -0,0 +1,39 @@
+﻿using StreamBoard.Core;
+using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
+
+namespace StreamBoard.Features.Decks.Models
+{
+    public class DeckButton : ObservableObject
+    {
+        private string _name = "";
+        [JsonPropertyName("name")]
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        private string _backgroundColor = "#FF2D2D2D";
+        [JsonPropertyName("background_color")]
+        public string BackgroundColor
+        {
+            get => _backgroundColor;
+            set => SetProperty(ref _backgroundColor, value);
+        }
+
+        private string? _imagePath;
+        [JsonPropertyName("image_path")]
+        public string? ImagePath
+        {
+            get => _imagePath;
+            set => SetProperty(ref _imagePath, value);
+        }
+
+        [JsonPropertyName("actions")]
+        public ObservableCollection<DeckAction> Actions { get; set; } = [];
+
+        [JsonIgnore]
+        public bool HasName => !string.IsNullOrWhiteSpace(Name);
+    }
+}
