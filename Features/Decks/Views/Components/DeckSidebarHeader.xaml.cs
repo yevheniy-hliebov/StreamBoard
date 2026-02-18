f39f1ccae9d90e82413c47e481e3b708f11c4051
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-02-18 10:55:58 +0200

    refactor(decks): extract sidebar header logic into reusable DeckSidebarHeader

---
+++ Features/Decks/Views/Components/DeckSidebarHeader.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StreamBoard.Features.Decks.Views.Components
+{
+    public partial class DeckSidebarHeader : UserControl
+    {
+        public DeckSidebarHeader() => InitializeComponent();
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(DeckSidebarHeader),
+                new PropertyMetadata("")
+            );
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty RightContentProperty =
+            DependencyProperty.Register(
+                nameof(RightContent),
+                typeof(object),
+                typeof(DeckSidebarHeader),
+                new PropertyMetadata(null)
+            );
+
+        public object RightContent
+        {
+            get => GetValue(RightContentProperty);
+            set => SetValue(RightContentProperty, value);
+        }
+
+        public static readonly DependencyProperty BottomContentProperty =
+            DependencyProperty.Register(
+                nameof(BottomContent),
+                typeof(object),
+                typeof(DeckSidebarHeader),
+                new PropertyMetadata(null)
+            );
+
+        public object BottomContent
+        {
+            get => GetValue(BottomContentProperty);
+            set => SetValue(BottomContentProperty, value);
+        }
+    }
+}
