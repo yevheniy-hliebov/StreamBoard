fd713726e2d49bf3323cf96d0a791ad8fcdfcdec
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-03-21 18:10:03 +0200

    chore: initialize Flutter client project for web and android

---
+++ Client/android/app/src/main/kotlin/com/streamboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.streamboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Client/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StreamBoard Application',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('StreamBoard Application')));
+  }
+}
