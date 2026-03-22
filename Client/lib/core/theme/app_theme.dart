95dbe0b31aee3e8953fc708c9e8d84357b29f81d
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-03-22 09:55:14 +0200

    feat(client-core): add AppColors and AppTheme, connect theme in main.dart

---
+++ Client/lib/core/theme/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:streamboard/core/constants/app_colors.dart';
+
+class AppTheme {
+  const AppTheme._();
+
+  static ThemeData light = _buildTheme(AppColors.light, Brightness.light);
+  static ThemeData dark = _buildTheme(AppColors.dark, Brightness.dark);
+
+  static ThemeData _buildTheme(AppColorsData colors, Brightness brightness) {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: colors.primary,
+        surface: colors.surface,
+        onSurface: colors.onSurface,
+        outline: colors.outline,
+        brightness: brightness,
+      ),
+      scaffoldBackgroundColor: colors.background,
+    );
+  }
+}
--- Client/lib/main.dart
+++ Client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:streamboard/core/theme/app_theme.dart';
 
 void main() {
   runApp(const App());
@@ -11,6 +12,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'StreamBoard Application',
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const HomeScreen(),
     );
