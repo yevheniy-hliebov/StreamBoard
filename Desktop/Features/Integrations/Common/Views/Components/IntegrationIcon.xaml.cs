ca6baff4a2aa8f5bd694883a72c5298ae2830dcd
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-03-27 10:36:36 +0200

    feat(integrations): add integration icon control

---
+++ Desktop/Features/Integrations/Common/Views/Components/IntegrationIcon.xaml.cs
@@ -0,0 +1,78 @@
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Wpf.Ui.Controls;
+using StreamBoard.Features.Integrations.Common.Models;
+
+namespace StreamBoard.Features.Integrations.Common.Views.Components
+{
+
+    public partial class IntegrationIcon : ImageIcon
+    {
+        public IntegrationIcon()
+        {
+            InitializeComponent();
+            UpdateIconSource(IconType);
+
+            Width = Size;
+            Height = Size;
+        }
+
+        public IntegrationIconType IconType
+        {
+            get => (IntegrationIconType)GetValue(IconTypeProperty);
+            set => SetValue(IconTypeProperty, value);
+        }
+
+        public static readonly DependencyProperty IconTypeProperty =
+            DependencyProperty.Register(
+                nameof(IconType),
+                typeof(IntegrationIconType),
+                typeof(IntegrationIcon),
+                new PropertyMetadata(default(IntegrationIconType), OnIconTypeChanged));
+
+        private static void OnIconTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is IntegrationIcon control && e.NewValue is IntegrationIconType type)
+            {
+                control.UpdateIconSource(type);
+            }
+        }
+
+        private void UpdateIconSource(IntegrationIconType type)
+        {
+            string fileName = type.ToString().ToLower();
+            string uriString = $"pack://application:,,,/Assets/Images/Integrations/{fileName}.png";
+
+            try
+            {
+                Source = new BitmapImage(new Uri(uriString));
+            }
+            catch (Exception)
+            {
+                Source = null;
+            }
+        }
+
+        public int Size
+        {
+            get => (int)GetValue(SizeProperty);
+            set => SetValue(SizeProperty, value);
+        }
+
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register(
+                nameof(Size),
+                typeof(int),
+                typeof(IntegrationIcon),
+                new PropertyMetadata(16, OnSizeChanged));
+
+        private static void OnSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is IntegrationIcon control && e.NewValue is int newSize)
+            {
+                control.Width = newSize;
+                control.Height = newSize;
+            }
+        }
+    }
+}
