7ba7cf7b633d94933130b7933a175bbff7c8294d
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-03-31 14:27:33 +0300

    feat(obs): enhance AppInitializer to support OBS auto-connect on startup

--- Desktop/Core/AppStartup/AppInitializer.cs
+++ Desktop/Core/AppStartup/AppInitializer.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.DependencyInjection;
 using StreamBoard.Features.Servers.Services;
 using StreamBoard.Features.Decks.Services;
 using System.Windows;
+using StreamBoard.Features.Integrations.Obs.Services;
+using StreamBoard.Features.Integrations.Common.Services;
 
 namespace StreamBoard.Core.AppStartup
 {
@@ -11,20 +13,23 @@ namespace StreamBoard.Core.AppStartup
     {
         public static async Task InitializeAsync(IServiceProvider serviceProvider)
         {
-            var storage = serviceProvider.GetRequiredService<SettingsStorage>();
+            var settingStorage = serviceProvider.GetRequiredService<SettingsStorage>();
             var privilegeService = serviceProvider.GetRequiredService<PrivilegeService>();
             var httpServer = serviceProvider.GetRequiredService<HttpServer>();
             var registry = serviceProvider.GetRequiredService<ActionRegistry>();
             var gridDeckStorage = serviceProvider.GetRequiredService<GridDeckStorage>();
             var keyboardDeckStorage = serviceProvider.GetRequiredService<KeyboardDeckStorage>();
 
+            var integrationStorage = serviceProvider.GetRequiredService<IntegrationConnectionStorage>();
+            var obsService = serviceProvider.GetRequiredService<ObsService>();
+
             // Theme
             ApplicationThemeManager.Apply(
-                storage.Current.Theme == "Dark" ? ApplicationTheme.Dark : ApplicationTheme.Light
+                settingStorage.Current.Theme == "Dark" ? ApplicationTheme.Dark : ApplicationTheme.Light
             );
 
             // Run as admin
-            if (storage.Current.RunAsAdmin && !privilegeService.IsRunAsAdmin())
+            if (settingStorage.Current.RunAsAdmin && !privilegeService.IsRunAsAdmin())
             {
                 privilegeService.RestartAsAdmin();
                 Application.Current.Shutdown();
@@ -34,6 +39,11 @@ namespace StreamBoard.Core.AppStartup
             // HttpServer start
             if (httpServer != null && httpServer.ShouldAutoStart && !httpServer.IsRunning)
                 await httpServer.Start();
+
+            if (integrationStorage.Current.Obs.AutoConnectOnStartup && !obsService.IsConnected)
+            {
+                obsService.Connect();
+            }
 
             // Actions
             registry.RegisterActions();
