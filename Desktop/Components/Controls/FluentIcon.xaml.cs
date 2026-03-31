3acd13e2b3a1ce63c152a3427bffb5ba9de13aa2
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-03-31 14:27:53 +0300

    refactor(controls): add IconGlyphAttribute to FluentIconType for improved glyph management

--- Desktop/Components/Controls/FluentIcon.xaml.cs
+++ Desktop/Components/Controls/FluentIcon.xaml.cs
@@ -1,41 +1,49 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
 using Wpf.Ui.Controls;
 
 namespace StreamBoard.Components.Controls
 {
+    [AttributeUsage(AttributeTargets.Field)]
+    public class IconGlyphAttribute(string glyph) : Attribute
+    {
+        public string Glyph { get; } = glyph;
+    }
+
     public enum FluentIconType
     {
-        Add,
-        Apps,
-        Delete,
-        Devices,
-        Document,
-        Checkbox,
-        ChevronDown,
-        ChevronRight,
-        ChromeClose,
-        ClearAllInk,
-        Color,
-        Folder,
-        Globe,
-        Grid,
-        GripperBarHorizontal,
-        Help,
-        Home,
-        Keyboard,
-        Lock,
-        Moon,
-        Network,
-        Photo,
-        PowerButton,
-        Puzzle,
-        Rename,
-        Search,
-        Settings,
-        Stop,
-        Sync,
-        System,
-        Timer,
+        [IconGlyph("\uE710")] Add,
+        [IconGlyph("\uED35")] Apps,
+        [IconGlyph("\uE74D")] Delete,
+        [IconGlyph("\uEA6C")] Devices,
+        [IconGlyph("\uE8A5")] Document,
+        [IconGlyph("\uE739")] Checkbox,
+        [IconGlyph("\uE70D")] ChevronDown,
+        [IconGlyph("\uE76C")] ChevronRight,
+        [IconGlyph("\uE8BB")] ChromeClose,
+        [IconGlyph("\uED62")] ClearAllInk,
+        [IconGlyph("\uE790")] Color,
+        [IconGlyph("\uE9A6")] FitPage,
+        [IconGlyph("\uE8B7")] Folder,
+        [IconGlyph("\uE12B")] Globe,
+        [IconGlyph("\uF0E2")] Grid,
+        [IconGlyph("\uE76F")] GripperBarHorizontal,
+        [IconGlyph("\uE897")] Help,
+        [IconGlyph("\uE80F")] Home,
+        [IconGlyph("\uE92E")] Keyboard,
+        [IconGlyph("\uE72E")] Lock,
+        [IconGlyph("\uE708")] Moon,
+        [IconGlyph("\uE968")] Network,
+        [IconGlyph("\uE91B")] Photo,
+        [IconGlyph("\uE7E8")] PowerButton,
+        [IconGlyph("\uEA86")] Puzzle,
+        [IconGlyph("\uE8AC")] Rename,
+        [IconGlyph("\uE71E")] Search,
+        [IconGlyph("\uE713")] Settings,
+        [IconGlyph("\uE71A")] Stop,
+        [IconGlyph("\uE895")] Sync,
+        [IconGlyph("\uE770")] System,
+        [IconGlyph("\uE916")] Timer
     }
 
     public partial class FluentIcon : FontIcon
@@ -53,8 +61,8 @@ namespace StreamBoard.Components.Controls
         }
 
         public static readonly DependencyProperty IconTypeProperty =
-        DependencyProperty.Register(nameof(IconType), typeof(FluentIconType), typeof(FluentIcon),
-            new PropertyMetadata(FluentIconType.Add, OnIconTypeChanged));
+            DependencyProperty.Register(nameof(IconType), typeof(FluentIconType), typeof(FluentIcon),
+                new PropertyMetadata(FluentIconType.Add, OnIconTypeChanged));
 
         public FluentIconType IconType
         {
@@ -72,41 +80,10 @@ namespace StreamBoard.Components.Controls
 
         private void UpdateGlyph(FluentIconType type)
         {
-            this.Glyph = type switch
-            {
-                FluentIconType.Add => "\uE710",
-                FluentIconType.Apps => "\uED35",
-                FluentIconType.Delete => "\uE74D",
-                FluentIconType.Devices => "\uEA6C",
-                FluentIconType.Document => "\uE8A5",
-                FluentIconType.Checkbox => "\uE739",
-                FluentIconType.ChevronDown => "\uE70D",
-                FluentIconType.ChevronRight => "\uE76C",
-                FluentIconType.ChromeClose => "\uE8BB",
-                FluentIconType.ClearAllInk => "\uED62",
-                FluentIconType.Color => "\uE790",
-                FluentIconType.Folder => "\uE8B7",
-                FluentIconType.Globe => "\uE12B",
-                FluentIconType.Grid => "\uF0E2",
-                FluentIconType.GripperBarHorizontal => "\uE76F",
-                FluentIconType.Help => "\uE897",
-                FluentIconType.Home => "\uE80F",
-                FluentIconType.Keyboard => "\uE92E",
-                FluentIconType.Lock => "\uE72E",
-                FluentIconType.Moon => "\uE708",
-                FluentIconType.Network => "\uE968",
-                FluentIconType.Photo => "\uE91B",
-                FluentIconType.PowerButton => "\uE7E8",
-                FluentIconType.Puzzle => "\uEA86",
-                FluentIconType.Rename => "\uE8AC",
-                FluentIconType.Search => "\uE71E",
-                FluentIconType.Settings => "\uE713",
-                FluentIconType.Stop => "\uE71A",
-                FluentIconType.Sync => "\uE895",
-                FluentIconType.System => "\uE770",
-                FluentIconType.Timer => "\uE916",
-                _ => "\uE710"
-            };
+            var field = type.GetType().GetField(type.ToString());
+            var attribute = field?.GetCustomAttribute<IconGlyphAttribute>();
+
+            this.Glyph = attribute?.Glyph ?? "\uE739";
         }
     }
 }
