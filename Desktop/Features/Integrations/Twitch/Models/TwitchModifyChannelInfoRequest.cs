a76860b8f8fd043cfb3e5c4068658c02c38158c6
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-04-06 15:46:17 +0300

    feat(twitch): implement channel management module

---
+++ Desktop/Features/Integrations/Twitch/Models/TwitchModifyChannelInfoRequest.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Serialization;
+
+namespace StreamBoard.Features.Integrations.Twitch.Models
+{
+    public class TwitchModifyChannelRequest
+    {
+        [JsonPropertyName("game_id")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? GameId { get; set; }
+
+        [JsonPropertyName("title")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("broadcaster_language")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? BroadcasterLanguage { get; set; }
+
+        [JsonPropertyName("tags")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string[]? Tags { get; set; }
+
+        [JsonPropertyName("is_branded_content")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public bool? IsBrandedContent { get; set; }
+    }
+}
