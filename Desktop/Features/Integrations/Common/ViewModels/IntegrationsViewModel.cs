6bea77179cbfa3e88f4991a03e9cf5fbd4c61082
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-04-11 13:36:16 +0300

    feat(twitch): implement TwitchIntegrationState for accounts tracking - create `TwitchIntegrationState` to map account auth status to `ConnectionState`; - register Broadcaster and Bot integration states in `IntegrationsViewModel`.

--- Desktop/Features/Integrations/Common/ViewModels/IntegrationsViewModel.cs
+++ Desktop/Features/Integrations/Common/ViewModels/IntegrationsViewModel.cs
@@ -3,6 +3,8 @@ using StreamBoard.Core;
 using StreamBoard.Features.Integrations.Common.Models;
 using StreamBoard.Features.Integrations.Obs.Models;
 using StreamBoard.Features.Integrations.Obs.Services;
+using StreamBoard.Features.Integrations.Twitch.Models;
+using StreamBoard.Features.Integrations.Twitch.Services;
 
 namespace StreamBoard.Features.Integrations.Common.ViewModels
 {
@@ -10,10 +12,12 @@ namespace StreamBoard.Features.Integrations.Common.ViewModels
     {
         public ObservableCollection<IntegrationStateModel> Integrations { get; set; }
 
-        public IntegrationsViewModel(ObsService obsService)
+        public IntegrationsViewModel(ObsService obsService, TwitchAccountsGateway twitchGateway)
         {
             Integrations = [
                 new ObsIntegrationState(obsService),
+                new TwitchIntegrationState(twitchGateway.Broadcaster),
+                new TwitchIntegrationState(twitchGateway.Bot)
             ];
         }
     }
