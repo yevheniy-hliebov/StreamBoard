b015f22c0587cd971e612c3a987155cb1d5c8899
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-04-28 10:31:48 +0300

    feat(ui): add admin status indicator to titlebar

---
+++ Desktop/Components/Controls/AdminStatusIcon.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StreamBoard.Features.Settings.Services;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StreamBoard.Components.Controls
+{
+    public partial class AdminStatusIcon : UserControl
+    {
+        public AdminStatusIcon()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnLoaded;
+
+            var privilegeService = App.ServiceProvider.GetRequiredService<PrivilegeService>();
+
+            if (privilegeService != null)
+            {
+                bool isAdmin = privilegeService.IsRunAsAdmin();
+                if (isAdmin)
+                {
+                    Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
