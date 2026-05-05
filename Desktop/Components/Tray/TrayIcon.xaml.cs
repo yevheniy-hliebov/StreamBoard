bf20803f4d72ca49bf17bccda68cd7e047b2e53b
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-05-05 12:01:46 +0300

    feat(tray): open a window with a single mouse click on the tray icon

--- Desktop/Components/Tray/TrayIcon.xaml.cs
+++ Desktop/Components/Tray/TrayIcon.xaml.cs
@@ -12,18 +12,31 @@ namespace StreamTabula.Components.Tray
 
         private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            var mainWindow = Application.Current.MainWindow;
-            if (mainWindow != null)
-            {
-                mainWindow.Show();
-                mainWindow.WindowState = WindowState.Normal;
-                mainWindow.Activate();
-            }
+            OpenApplication();
+        }
+
+        private void NotifyIcon_TrayLeftMouseDown(object sender, RoutedEventArgs e)
+        {
+            OpenApplication();
         }
 
         private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private static void OpenApplication()
+        {
+            var mainWindow = Application.Current.MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.Show();
+                if (mainWindow.WindowState == WindowState.Minimized)
+                {
+                    mainWindow.WindowState = WindowState.Normal;
+                }
+                mainWindow.Activate();
+            }
         }
     }
 }
