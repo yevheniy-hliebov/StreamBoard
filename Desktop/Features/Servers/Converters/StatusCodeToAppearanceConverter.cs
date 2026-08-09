561bf3b8973a19e36630ad8e396364d616f3e990
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-08-09 15:09:28 +0300

    refactor(servers): optimize StatusCodeToAppearanceConverter and move to Features
    - moved StatusCodeToAppearanceConverter from Helpers to StreamTabula.Features.Servers.Converters;
    - rewrote the converter logic using modern C# pattern matching (switch expressions) to improve readability and remove nested ifs;
    - updated xmlns in HttpRequestLogItem to reference the new converter location.

---
+++ Desktop/Features/Servers/Converters/StatusCodeToAppearanceConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using Wpf.Ui.Controls;
+
+namespace StreamTabula.Features.Servers.Converters;
+
+public class StatusCodeToAppearanceConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not int code)
+            return ControlAppearance.Secondary;
+
+        return code switch
+        {
+            >= 200 and < 300 => ControlAppearance.Success,
+            >= 300 and < 400 => ControlAppearance.Info,
+            >= 400 and < 500 => ControlAppearance.Caution,
+            >= 500 => ControlAppearance.Danger,
+            _ => ControlAppearance.Secondary
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
