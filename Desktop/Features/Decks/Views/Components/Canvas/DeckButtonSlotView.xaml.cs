1f534ae951695ffdd1ed9ea16958f79978ebb2e6
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-08-29 01:44:53 +0300

    refactor(decks): optimize deck button rendering and fix click bindings
    - rewrite `DeckButtonSlotView` using a 3-layer `Grid` and `ImageBrush` to eliminate anti-aliasing artifacts on rounded corners;
    - centralize individual visual properties into a single `Config` dependency property to streamline XAML bindings and code-behind;
    - implement dynamic `PropertyChanged` subscriptions in the button slot to actively track missing image status;
    - update `ClippingBorder` geometry calculations to accurately subtract border thickness and padding;
    - fix broken click commands in `GridCanvasView` by using `RelativeSource` instead of `ElementName` to avoid namescope collisions.

--- Desktop/Features/Decks/Views/Components/Canvas/DeckButtonSlotView.xaml.cs
+++ Desktop/Features/Decks/Views/Components/Canvas/DeckButtonSlotView.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System.ComponentModel;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using StreamTabula.Features.Decks.Models;
 
 namespace StreamTabula.Features.Decks.Views.Components.Canvas
 {
@@ -14,78 +16,50 @@ namespace StreamTabula.Features.Decks.Views.Components.Canvas
             get => (bool)GetValue(IsSelectedProperty);
             set => SetValue(IsSelectedProperty, value);
         }
-
         public static readonly DependencyProperty IsSelectedProperty =
             DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(DeckButtonSlotView), new PropertyMetadata(false));
 
-
         public ICommand Command
         {
-            get { return (ICommand)GetValue(CommandProperty); }
-            set { SetValue(CommandProperty, value); }
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
         }
-
         public static readonly DependencyProperty CommandProperty =
             DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(DeckButtonSlotView), new PropertyMetadata(null));
 
-
         public object CommandParameter
         {
-            get { return (object)GetValue(CommandParameterProperty); }
-            set { SetValue(CommandParameterProperty, value); }
+            get => (object)GetValue(CommandParameterProperty);
+            set => SetValue(CommandParameterProperty, value);
         }
-
         public static readonly DependencyProperty CommandParameterProperty =
             DependencyProperty.Register(nameof(CommandParameter), typeof(object), typeof(DeckButtonSlotView), new PropertyMetadata(null));
 
-
-        public string ButtonName
-        {
-            get { return (string)GetValue(ButtonNameProperty); }
-            set { SetValue(ButtonNameProperty, value); }
-        }
-
-        public static readonly DependencyProperty ButtonNameProperty =
-            DependencyProperty.Register(nameof(ButtonName), typeof(string), typeof(DeckButtonSlotView), new PropertyMetadata(""));
-
-
         public int ButtonIndex
         {
-            get { return (int)GetValue(ButtonIndexProperty); }
-            set { SetValue(ButtonIndexProperty, value); }
+            get => (int)GetValue(ButtonIndexProperty);
+            set => SetValue(ButtonIndexProperty, value);
         }
-
         public static readonly DependencyProperty ButtonIndexProperty =
             DependencyProperty.Register(nameof(ButtonIndex), typeof(int), typeof(DeckButtonSlotView), new PropertyMetadata(0));
 
-
-        public string ImagePath
+        public DeckButtonConfig? Config
         {
-            get { return (string)GetValue(ImagePathProperty); }
-            set { SetValue(ImagePathProperty, value); }
+            get => (DeckButtonConfig?)GetValue(ConfigProperty);
+            set => SetValue(ConfigProperty, value);
         }
 
-        public static readonly DependencyProperty ImagePathProperty =
+        public static readonly DependencyProperty ConfigProperty =
             DependencyProperty.Register(
-                nameof(ImagePath),
-                typeof(string),
+                nameof(Config),
+                typeof(DeckButtonConfig),
                 typeof(DeckButtonSlotView),
-                new PropertyMetadata(null, OnImagePathChanged));
-
-
-        public string ButtonBackground
-        {
-            get { return (string)GetValue(ButtonBackgroundProperty); }
-            set { SetValue(ButtonBackgroundProperty, value); }
-        }
-
-        public static readonly DependencyProperty ButtonBackgroundProperty =
-            DependencyProperty.Register(nameof(ButtonBackground), typeof(string), typeof(DeckButtonSlotView), new PropertyMetadata("#2B2B2B"));
+                new PropertyMetadata(null, OnConfigChanged));
 
         public bool IsImageMissing
         {
-            get { return (bool)GetValue(IsImageMissingProperty); }
-            private set { SetValue(IsImageMissingPropertyKey, value); }
+            get => (bool)GetValue(IsImageMissingProperty);
+            private set => SetValue(IsImageMissingPropertyKey, value);
         }
 
         private static readonly DependencyPropertyKey IsImageMissingPropertyKey =
@@ -97,21 +71,38 @@ namespace StreamTabula.Features.Decks.Views.Components.Canvas
 
         public static readonly DependencyProperty IsImageMissingProperty = IsImageMissingPropertyKey.DependencyProperty;
 
-        private static void OnImagePathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnConfigChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is DeckButtonSlotView slotView)
             {
-                string? path = e.NewValue as string;
-
-                if (!string.IsNullOrWhiteSpace(path))
+                if (e.OldValue is DeckButtonConfig oldConfig)
                 {
-                    slotView.IsImageMissing = !File.Exists(path);
+                    oldConfig.PropertyChanged -= slotView.Config_PropertyChanged;
+                }
+
+                if (e.NewValue is DeckButtonConfig newConfig)
+                {
+                    newConfig.PropertyChanged += slotView.Config_PropertyChanged;
+                    slotView.UpdateImageStatus(newConfig.ImagePath);
                 }
                 else
                 {
-                    slotView.IsImageMissing = false;
+                    slotView.UpdateImageStatus(null);
                 }
             }
+        }
+
+        private void Config_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(DeckButtonConfig.ImagePath) && sender is DeckButtonConfig config)
+            {
+                UpdateImageStatus(config.ImagePath);
+            }
+        }
+
+        private void UpdateImageStatus(string? path)
+        {
+            IsImageMissing = !string.IsNullOrWhiteSpace(path) && !File.Exists(path);
         }
     }
 }
