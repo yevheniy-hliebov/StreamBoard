8983d57ad794eff7b37de0185eb2f90340610480
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-09-02 17:29:37 +0300

    perf(dropdown-field): optimize option refresh logic to prevent redundant UI updates
    - ignore `BaseAction.Label` property changes to avoid triggering `RefreshOptions()` on every keystroke during text input;
    - bypass refresh triggers for `Property.Name` modifications as they do not affect available dropdown items.

--- Desktop/Features/Actions/ViewModels/Fields/DropdownFieldViewModel.cs
+++ Desktop/Features/Actions/ViewModels/Fields/DropdownFieldViewModel.cs
@@ -5,95 +5,100 @@ using System.Windows.Input;
 using StreamTabula.Features.Actions.Models;
 using Wpf.Ui.Input;
 
-namespace StreamTabula.Features.Actions.ViewModels
+namespace StreamTabula.Features.Actions.ViewModels;
+
+public class DropdownFieldViewModel : ActionFieldViewModel
 {
-    public class DropdownFieldViewModel : ActionFieldViewModel
+    private readonly IOptionsProvider _provider;
+    private readonly BaseAction _action;
+
+    public ObservableCollection<DropdownOption> Options { get; } = new();
+
+    public DropdownFieldViewModel(
+        string label,
+        string? hint,
+        BaseAction targetAction,
+        PropertyInfo property,
+        IOptionsProvider provider)
+        : base(label, hint, targetAction, property)
     {
-        private readonly IOptionsProvider _provider;
-        private readonly BaseAction _action;
+        _action = targetAction;
+        _provider = provider;
 
-        public ObservableCollection<DropdownOption> Options { get; } = new();
+        _action.PropertyChanged += OnActionPropertyChanged;
 
-        public DropdownFieldViewModel(
-            string label,
-            string? hint,
-            BaseAction targetAction,
-            PropertyInfo property,
-            IOptionsProvider provider)
-            : base(label, hint, targetAction, property)
+        RefreshOptions();
+    }
+
+    public object? Value
+    {
+        get => Property.GetValue(TargetAction);
+        set
         {
-            _action = targetAction;
-            _provider = provider;
-
-            _action.PropertyChanged += OnActionPropertyChanged;
-
-            RefreshOptions();
-        }
-
-        public object? Value
-        {
-            get => Property.GetValue(TargetAction);
-            set
+            if (!Equals(Value, value))
             {
-                if (!Equals(Value, value))
-                {
-                    Property.SetValue(TargetAction, value);
-                    OnPropertyChanged();
-                    OnPropertyChanged(nameof(SelectedDisplayOption));
-                }
+                Property.SetValue(TargetAction, value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedDisplayOption));
+            }
+        }
+    }
+
+    public string? SelectedDisplayOption
+    {
+        get
+        {
+            var currentVal = Value;
+            if (currentVal == null || (currentVal is string s && string.IsNullOrWhiteSpace(s)))
+                return null;
+
+            var selectedOpt = Options.FirstOrDefault(o => Equals(o.Value, currentVal));
+            return selectedOpt?.DisplaySelectedOption ?? currentVal.ToString();
+        }
+    }
+
+    public ICommand SelectOptionCommand => new RelayCommand<DropdownOption>(option =>
+    {
+        if (option != null)
+        {
+            Value = option.Value;
+        }
+    });
+
+    private void OnActionPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(BaseAction.Label))
+            return;
+
+        if (e.PropertyName == Property.Name)
+            return;
+
+        RefreshOptions();
+    }
+
+    private void RefreshOptions()
+    {
+        var rawOptions = _provider.GetOptions(_action) ?? [];
+
+        Options.Clear();
+
+        foreach (var opt in rawOptions)
+        {
+            if (opt is DropdownOption dropOption)
+            {
+                Options.Add(dropOption);
+            }
+            else if (opt is string strOption)
+            {
+                Options.Add(new DropdownOption(strOption));
+            }
+            else if (opt != null)
+            {
+                Options.Add(new DropdownOption(opt));
             }
         }
 
-        public string? SelectedDisplayOption
-        {
-            get
-            {
-                var currentVal = Value;
-                if (currentVal == null || (currentVal is string s && string.IsNullOrWhiteSpace(s)))
-                    return null;
-
-                var selectedOpt = Options.FirstOrDefault(o => Equals(o.Value, currentVal));
-                return selectedOpt?.DisplaySelectedOption ?? currentVal.ToString();
-            }
-        }
-
-        public ICommand SelectOptionCommand => new RelayCommand<DropdownOption>(option =>
-        {
-            if (option != null)
-            {
-                Value = option.Value;
-            }
-        });
-
-        private void OnActionPropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            RefreshOptions();
-        }
-
-        private void RefreshOptions()
-        {
-            var rawOptions = _provider.GetOptions(_action) ?? [];
-
-            Options.Clear();
-
-            foreach (var opt in rawOptions)
-            {
-                if (opt is DropdownOption dropOption)
-                {
-                    Options.Add(dropOption);
-                }
-                else if (opt is string strOption)
-                {
-                    Options.Add(new DropdownOption(strOption));
-                }
-                else if (opt != null)
-                {
-                    Options.Add(new DropdownOption(opt));
-                }
-            }
-
-            OnPropertyChanged(nameof(Options));
-            OnPropertyChanged(nameof(SelectedDisplayOption));
-        }
+        OnPropertyChanged(nameof(Options));
+        OnPropertyChanged(nameof(SelectedDisplayOption));
     }
 }
